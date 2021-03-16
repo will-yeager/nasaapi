2c1f90d7e94ff741754df1b260e3b505469e6135
William Jonathan <78751043+will-yeager@users.noreply.github.com>
2021-03-16 01:40:04 -0300

    Update api.py

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ import os
 import glob
 
 # api_key = PARA USAR ESSE PROGRAMA É NECESSARIO GERA UMA API KEY NO SITE DA NASA --> https://api.nasa.gov/index.html#apply-for-an-api-key
-api_key = 'nkDmhK5wuXwCOtGzZ6Pp1OE7ES9HxMfVkD7Hv5I4'
+api_key = 'API-KEY' # <-- INSIRA SUA API KEY EM API-KEY
 # DIRETORIO = r'SEU DIRETORIO' por exemplo -- > r'C:\Users\XXXX\Desktop\Nasaapi\imgs'
 diretorio = r"C:\Users\XXX\Desktop\Nasaapi\\"
 
